390e95ddfdfa34a9b61fff8adea53f3ef0f12ef3
LesunenkoNikita <nikitoslesunenko@gmail.com>
2024-05-27 01:23:51 +0300

    impemented function for allocation of memory for array

--- Assignment1/Assignment1/main.cpp
+++ Assignment1/Assignment1/main.cpp
@@ -1,6 +1,20 @@
 #include <stdio.h> 
+#include <stdlib.h>
 
-void print_info(void) 
+size_t bufferSize = 256;
+char** text;
+int rowsAmount = 10;
+
+void array2dMalloc(char** arr) 
+{
+	arr = (char**)malloc(rowsAmount * sizeof(char*));
+	for (int i = 0; i < rowsAmount; i++)
+	{
+		arr[i] = (char*)calloc(bufferSize, sizeof(char));
+	}
+}
+
+void printInfo(void) 
 { 
 	printf("All available commands:\n\
 1 - To append text symbols to the end\n\
@@ -11,8 +25,15 @@ void print_info(void)
 6 - To insert the text by line and symbol index\n\
 7 - To search text");
 } 
+
+
 int main(void) 
 { 
-	print_info();
+	array2dMalloc(text);
+	printInfo();
+	char* input = (char*)malloc(bufferSize * sizeof(char));
+	printf("Enter a number: ");
+	fgets(input, bufferSize, stdin);
+	//free(input);
 	return 0; 
 }
