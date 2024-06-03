2a64bf71e0bd8159801c9efb52e7c5eec8cd558c
LesunenkoNikita <nikitoslesunenko@gmail.com>
2024-06-03 13:50:33 +0300

    fixed appendText functio, which now works properly

--- Assignment1/Assignment1/main.cpp
+++ Assignment1/Assignment1/main.cpp
@@ -1,3 +1,4 @@
+#define _CTR_SECURE_NO_WARNINGS
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
@@ -8,13 +9,14 @@ int columnAmount = 256;
 int rowsAmount = 10;
 int curRow = 0;
 
-void array2dMalloc(char** arr) 
+void array2dMalloc(void) 
 {
-	arr = (char**)malloc(rowsAmount * sizeof(char*));
+	text = (char**)malloc(rowsAmount * sizeof(char*));
 	for (int i = 0; i < rowsAmount-1; i++)
 	{
-		arr[i] = (char*)calloc(columnAmount, sizeof(char));
+		text[i] = (char*)malloc(columnAmount * sizeof(char));
 	}
+	text[0][0] = '\0';
 }
 
 void printInfo(void) 
@@ -26,7 +28,7 @@ void printInfo(void)
 4 - To load the text from the file\n\
 5 - To print current text to console\n\
 6 - To insert the text by line and symbol index\n\
-7 - To search text");
+7 - To search text\n");
 } 
 
 void appendText(void)
@@ -35,79 +37,69 @@ void appendText(void)
 	char* input = (char*)malloc(bufferSize * sizeof(char));
 	fgets(input, bufferSize, stdin);
 	int idx = 0;
-	for (idx; idx < sizeof(text[curRow] - 1); idx++)
+	for (idx; idx < strlen(text[curRow] - 1); idx++)
 	{
 		if (text[curRow][idx] == '\0')
 		{
 			break;
 		}
 	}
-	for (int i = 0; i < sizeof(input)-1; i++)
+	for (int i = 0; i < strlen(input)-1; i++)
 	{
 		text[curRow][idx] =input[i];
 		idx++;
 	}
+	text[curRow][idx] = '\0';
 	free(input);
 }
 
 void newLine(void)	
 {
 	curRow++;
-	printf("New line is started");
+	printf("New line is started!\n");
 }
 
 void textPrint(void)
 {
 	for (int r = 0; r <= curRow; r++)
 	{
-		/*for (int i = 0; i < sizeof(text[r]) - 1; i++)
-		{
-			if (text[r][i] == '\0')
-			{
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%c", text[r][i]);
-			}
-		}*/
-		printf("%s\n", text[r]);
+		printf("%s", text[r]);
 	}
+	printf("\n");
 }
 
-void saveToFile(void)
-{
-	printf("Enter the path to file: ");
-	char path[bufferSize];
-	fgets(path, bufferSize, stdin);
-	FILE* file = fopen(path, "w");
-	if (file != NULL)
-	{
-		for (int r = 0; r <= curRow; r++)
-		{
-			fputs((text[r],"\n"), file);
-		}
-	}
-	fclose(file);
-}
-
-void loadFromFile(void)
-{
-	printf("Enter the path to file: ");
-	char path[bufferSize];
-	fgets(path, bufferSize, stdin);
-	char lineFromFile[bufferSize];
-	int row = 0;
-	FILE* file = fopen(path, "r");
-	while (fgets(lineFromFile, bufferSize, file) != NULL) 
-	{
-		strcpy(text[row], lineFromFile);
-		row++;
-	}
-	curRow = row;
-	fclose(file);
-}
+//void saveToFile(void)
+//{
+//	printf("Enter the path to file: ");
+//	char path[bufferSize];
+//	fgets(path, bufferSize, stdin);
+//	FILE* file = fopen(path, "w");
+//	if (file != NULL)
+//	{
+//		for (int r = 0; r <= curRow; r++)
+//		{
+//			fputs((text[r],"\n"), file);
+//		}
+//	}
+//	fclose(file);
+//}
+//
+//void loadFromFile(void)
+//{
+//	printf("Enter the path to file: ");
+//	char path[bufferSize];
+//	fgets(path, bufferSize, stdin);
+//	char lineFromFile[bufferSize];
+//	int row = 0;
+//	FILE* file = fopen(path, "r");
+//	while (fgets(lineFromFile, bufferSize, file) != NULL) 
+//	{
+//		strcpy(text[row], lineFromFile);
+//		row++;
+//	}
+//	curRow = row;
+//	fclose(file);
+//}
 
 void search(void)
 {
@@ -117,11 +109,11 @@ void search(void)
 	int i = 0;
 	for (int r = 0; r < curRow; r++)
 	{
-		for (int j = 0; j < sizeof(text[r]) - 1; j++) 
+		for (int j = 0; j < strlen(text[r]) - 1; j++) 
 		{
 			if (text[r][j] == word[i])
 			{
-				if (i == sizeof(word)-1)
+				if (i == strlen(word)-1)
 				{
 					printf("The word you search is in row %d starting on index %d", r, j-sizeof(word)+1);
 					i = 0;
@@ -139,14 +131,24 @@ void search(void)
 	}
 }
 
+//void insertText(void) 
+//{
+//	
+//}
+
 int main(void) 
 { 
 	//to make a dynamic array!
-	array2dMalloc(text);
-	printInfo();
-	char input[16];
+	array2dMalloc();
+	//printInfo();
+	/*char input[16];
 	printf("Enter a number: ");
-	fgets(input, bufferSize, stdin);
+	fgets(input, bufferSize, stdin);*/
+	appendText();
+	//newLine();
+	appendText();
+	textPrint();
+	search();
 	//free(input);
 	return 0; 
 }
